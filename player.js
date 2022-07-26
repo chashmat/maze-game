731d413204145a6c25dc5d057dfd50d6860e295e
Chashmat un Nasim <chashmatnasim@gmail.com>
2022-07-26 18:16:39 +0500

    made a player

--- player.js
+++ player.js
@@ -1,2 +1,12 @@
-let maze = document.getElementsByTagName('canvas')[0];
-let ctx = maze.getContext('2d');
+let player = new Image();
+player.src = "./player.png";
+
+player.onload = () => {
+      let w = newMaze.size / newMaze.rows;
+      let nw = player.naturalWidth;
+      let nh = player.naturalHeight;
+      let ratio = nw / nh;
+      let h = w / ratio;
+
+      ctx.drawImage(player, 1, 1, w - 1, h - 1);
+};
